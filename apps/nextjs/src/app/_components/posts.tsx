fadf0d176b6f7c4ee850c4c2b6cdacd7fdcd1a91
jon <906671+jondwillis@users.noreply.github.com>
2023-11-09 08:36:40 -0800

    landing page

--- apps/nextjs/src/app/_components/posts.tsx
+++ apps/nextjs/src/app/_components/posts.tsx
@@ -1,74 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import * as React from "react";
 
-import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
 
-export function CreatePostForm() {
-  const context = api.useUtils();
-
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-
-  const { mutateAsync: createPost, error } = api.post.create.useMutation({
-    async onSuccess() {
-      setTitle("");
-      setContent("");
-      await context.post.all.invalidate();
-    },
-  });
-
-  return (
-    <form
-      className="flex w-full max-w-2xl flex-col"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        try {
-          await createPost({
-            title,
-            content,
-          });
-          setTitle("");
-          setContent("");
-          await context.post.all.invalidate();
-        } catch {
-          // noop
-        }
-      }}
-    >
-      <input
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Title"
-      />
-      {error?.data?.zodError?.fieldErrors.title && (
-        <span className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.title}
-        </span>
-      )}
-      <input
-        className="mb-2 rounded bg-white/10 p-2 text-white"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Content"
-      />
-      {error?.data?.zodError?.fieldErrors.content && (
-        <span className="mb-2 text-red-500">
-          {error.data.zodError.fieldErrors.content}
-        </span>
-      )}
-      {}
-      <button type="submit" className="rounded bg-pink-400 p-2 font-bold">
-        Create
-      </button>
-      {error?.data?.code === "UNAUTHORIZED" && (
-        <span className="mt-2 text-red-500">You must be logged in to post</span>
-      )}
-    </form>
-  );
-}
+// import GoogleIcon from "./GoogleIcon";
 
 export function PostList() {
   const [posts] = api.post.all.useSuspenseQuery();
@@ -101,7 +38,6 @@ export function PostCard(props: {
 }) {
   const context = api.useUtils();
   const deletePost = api.post.delete.useMutation();
-
   return (
     <div className="flex flex-row rounded-lg bg-white/10 p-4 transition-all hover:scale-[101%]">
       <div className="flex-grow">
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,38 +1,33 @@
 import { Suspense } from "react";
 
 import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
+
+import "./_components/posts";
+
+import { PostCardSkeleton, PostList } from "./_components/posts";
+import SignInSide from "./_components/signInSide";
 
 export const runtime = "edge";
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
-        <AuthShowcase />
+    // <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    // <AuthShowcase />
 
-        <CreatePostForm />
-        <div className="h-[40vh] w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList />
-          </Suspense>
-        </div>
-      </div>
-    </main>
+    <SignInSide />
+    // <div className="h-[40vh] w-full max-w-2xl overflow-y-scroll">
+    //   <Suspense
+    //     fallback={
+    //       <div className="flex w-full flex-col gap-4">
+    //         <PostCardSkeleton />
+    //         <PostCardSkeleton />
+    //         <PostCardSkeleton />
+    //       </div>
+    //     }
+    //   >
+    //     <PostList />
+    //   </Suspense>
+    // </div>
+    // </main>
   );
 }
